199a5414725f4a6292ef07eac02a5db7cf9899e7
michail-konon <greateyuri@gamil.com>
2023-04-01 19:33:43 +0300

    feat: add burger menu

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-console.log('1aefjf')
+const burger = document.querySelector('.lines');
+const overlay = document.querySelector('.overlay');
+const menue = document.querySelector('.navigation-small');
+const targetBody = document.querySelector('.body');
+const targetItem = document.querySelector('.nav-list-small')
+
+burger.addEventListener('click', () => {
+    close();
+    
+});
+
+overlay.addEventListener('click', () => {
+    close();
+    
+});
+
+targetItem.addEventListener('click', () => {
+    close();
+});
+
+function close() {
+    burger.classList.toggle('action');
+    overlay.classList.toggle('show');
+    menue.classList.toggle('show-menu');
+    targetBody.classList.toggle('unscroll')
+}
