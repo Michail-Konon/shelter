c07f4cacbda4e607c9558df2e254fcc8a8d8d22a
michail-konon <greateyuri@gamil.com>
2023-04-06 20:08:56 +0300

    feat: add popup prototype

--- script/script.js
+++ script/script.js
@@ -1,17 +1,22 @@
+import CARDS from '../pets.js'
+console.log(CARDS)
 const burger = document.querySelector('.lines');
 const overlay = document.querySelector('.overlay');
 const menue = document.querySelector('.navigation-small');
 const targetBody = document.querySelector('.body');
 const targetItem = document.querySelector('.nav-list-small')
 
+const cardOverlay = document.querySelector('.card-overlay');
+const petCard = document.querySelector('.slider-card');
+
+/*Minimise menu START*/
+
 burger.addEventListener('click', () => {
     close();
-    
 });
 
 overlay.addEventListener('click', () => {
     close();
-    
 });
 
 targetItem.addEventListener('click', () => {
@@ -24,3 +29,54 @@ function close() {
     menue.classList.toggle('show-menu');
     targetBody.classList.toggle('unscroll')
 }
+
+/*Minimise menu END*/
+
+/*Popup START*/
+
+petCard.addEventListener('click', () => {
+    closePopup();
+});
+
+cardOverlay.addEventListener('click', () => {
+    closePopup();
+});
+
+function closePopup() {
+    overlay.classList.toggle('show');
+    targetBody.classList.toggle('unscroll');
+}
+
+function popupMsg () {
+    
+}
+
+/*Popup END*/
+
+/*Unique arrray generation START*/
+// generate 8 unique item array
+// may cause system overload
+let arr = [];
+let set = new Set();
+
+function test () {
+    let set = new Set();
+	for(let i = 0; i < i+1; i++) {
+  	    if(set.size == 8) {
+        break
+    }
+    set.add(Math.floor(Math.random() * (8 - 0) + 0))
+  }
+  return arr = Array.from(set)
+}
+test ();
+console.log(arr);
+
+/*Unique arrray generation END*/
+
+/*Card generation START*/
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+  });
+
+/*Card generation END*/
